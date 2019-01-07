ecf608ec022ed0f9369367037f337e0f8facb51d
yahya-pondokit <yahya.left@gmail.com>
2019-01-07 12:38:37 +0700

    backup

---
+++ admin/layouts/sidebar.php
@@ -0,0 +1,20 @@
+<ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MAIN NAVIGATION</li>
+        <li class="active">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+          </a>
+        </li>
+        <li>
+          <a href="http://localhost/adminlte/admin/kategori">
+            <i class="fa fa-pie-chart"></i>
+            <span>Kategori</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fa fa-pie-chart"></i>
+            <span>Artikel</span>
+          </a>
+        </li>      
+</ul>
